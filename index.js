c9c4176d7e5b796de1369d2e89cf8c0dcbc9eb46
Balasubramanian Sankareswaran <Balasubramanian.Sankareswaran@quantium.com.au>
2019-09-02 22:31:26 +0530

    feat: added redis

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const redis = require("redis");
+
 const app = express();
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379
+});
+client.set("visits", 0);
 
 app.get('/', (req, res) => {
-  res.send("hi there");
+  client.get("visits",(err, visits) => {
+    res.send("Number of visits: "+visits);
+    client.set("visits", parseInt(visits)+1);
+  })
 })
 
 app.listen(8080, ()=>{
